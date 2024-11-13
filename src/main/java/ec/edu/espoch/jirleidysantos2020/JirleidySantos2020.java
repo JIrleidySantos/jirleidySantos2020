582e304d6fcbbdbdeeae1d8f74c34f8ea71cfa00
JIrleidySantos <jirleidysantosm@gmail.com>
2024-11-13 13:20:36 -0500

    Practica

---
+++ src/main/java/ec/edu/espoch/jirleidysantos2020/JirleidySantos2020.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.jirleidysantos2020;
+
+/**
+ *
+ * @author mundo
+ */
+public class JirleidySantos2020 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
